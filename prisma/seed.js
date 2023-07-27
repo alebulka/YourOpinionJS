2e2cae4dba37549bca284fd6c251aed047324184
alebulka <bulkalessandra@outlook.com>
2023-07-27 15:53:32 -0300

    refactor: bcrypt no arquivo seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,18 +1,27 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-const { hash } = require('./bcrypt.js');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 async function clearDB(){
   await prisma.opinions.deleteMany({where:{}});
-  await prisma.users.deleteMany({where:{}})
+  await prisma.users.deleteMany({where:{}});
 }
+
+async function hashPassword(password) {
+  return bcrypt.hash(password, saltRounds);
+}
+
 async function populateDB(){
-  const hashedPassword = await hash('teste123');
+  const hashedPassword1 = await hashPassword('teste123');
+  const hashedPassword2 = await hashPassword('isso aqui é outro teste');
+  const hashedPassword3 = await hashPassword('12345678');
 
   await prisma.users.createMany({
     data: [
-      {id: 1, nome:'Alessandra', email:'alessandrabulkar@gmail.com', cpf:123, senha: hashedPassword},
-      {id: 2, nome:'Paulo', email:'paulinho@gmail.com', cpf:234, senha: hashedPassword},
+      {id: 1, nome:'Alessandra', email:'bulkalessandra@outlook.com', cpf:123, senha: hashedPassword1},
+      {id: 2, nome:'Paulo', email:'paulinho@gmail.com', cpf:234, senha: hashedPassword2},
+      {id: 3, nome:'Mayra', email:'maybulka@gmail.com', cpf:389, senha: hashedPassword3}
     ]
   })
   await prisma.opinions.createMany({
